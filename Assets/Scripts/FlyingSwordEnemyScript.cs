3946391d177939a14b86125f753665baf43c5dbd
Marcus <mstierne@ucsc.edu>
2026-03-02 23:06:23 -0800

    flying sword enemies added

---
+++ Assets/Scripts/FlyingSwordEnemyScript.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+
+public class FlyingSwordEnemyScript : MonoBehaviour
+{
+    public float speed = 3f;
+    public float damage = 1f;
+
+    private Transform player;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (player == null) return;
+
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.linearVelocity = direction * speed;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle + 150f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("collide");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            UpdateHealth health = collision.gameObject.GetComponent<UpdateHealth>();
+            if (health != null)
+            {
+                health.TakeDamage(10, transform.position);
+            }
+        }
+    }
+}
