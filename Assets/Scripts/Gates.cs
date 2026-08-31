429defdd70e8cc4b5162242edbe5572950600e37
Benjamin Saavedra Gomes <bsgomes@ucsc.edu>
2026-08-30 20:29:32 -0700

    gate works, just needs sounds, animations, and different interact key image

--- Assets/Scripts/Gates.cs
+++ Assets/Scripts/Gates.cs
@@ -1,16 +1,133 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
-public class Gates : MonoBehaviour
+[RequireComponent(typeof(BoxCollider2D))]
+public class Gates : MonoBehaviour, IInteractable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject interactionPrompt;
+    [SerializeField] private string destinationSceneName = "InsideDojo";
+    [SerializeField] private string loadingScreenSceneName = "LoadingScreen";
+    [SerializeField, Min(0f)] private float activationThreshold = 0.5f;
+
+    private InputSystem_Actions inputActions;
+    private bool playerInRange;
+    private InteractPromptDisplay interactPromptDisplay;
+
+    private void Awake()
     {
-        
+        BoxCollider2D gateCollider = GetComponent<BoxCollider2D>();
+        if (gateCollider != null)
+        {
+            gateCollider.isTrigger = true;
+        }
+
+        inputActions = new InputSystem_Actions();
+        InputBindingOverrides.ApplySavedOverrides(inputActions.asset);
+        InputBindingOverrides.RegisterRuntimeGameplayAsset(inputActions.asset);
+
+        if (interactionPrompt != null)
+        {
+            interactPromptDisplay = interactionPrompt.GetComponent<InteractPromptDisplay>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputActions?.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions?.Gameplay.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        if (inputActions != null)
+        {
+            InputBindingOverrides.UnregisterRuntimeGameplayAsset(inputActions.asset);
+        }
+    }
+
+    private void Update()
+    {
+        UpdatePromptState();
+
+        if (!playerInRange)
+        {
+            return;
+        }
+
+        if (GameplayInputGate.BlocksGameplayActions)
+        {
+            return;
+        }
+
+        if (inputActions == null || !CanInteract())
+        {
+            return;
+        }
+
+        Vector2 moveInput = inputActions.Gameplay.Move.ReadValue<Vector2>();
+        if (moveInput.y >= activationThreshold)
+        {
+            Interact();
+        }
+    }
+
+    public void Interact()
+    {
+        if (!CanInteract())
+        {
+            return;
+        }
+
+        SceneTransition.SetPendingNextScene(destinationSceneName);
+        SceneManager.LoadScene(loadingScreenSceneName);
+    }
+
+    public bool CanInteract()
+    {
+        return playerInRange;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        playerInRange = true;
+        UpdatePromptState();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        playerInRange = false;
+        UpdatePromptState();
+    }
+
+    private void UpdatePromptState()
+    {
+        if (interactionPrompt == null)
+        {
+            return;
+        }
+
+        bool showPrompt = playerInRange && !GameplayInputGate.BlocksGameplayActions;
+        interactionPrompt.SetActive(showPrompt);
+
+        if (showPrompt)
+        {
+            interactPromptDisplay ??= interactionPrompt.GetComponent<InteractPromptDisplay>();
+            interactPromptDisplay?.Refresh();
+        }
     }
 }
